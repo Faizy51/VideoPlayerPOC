aad3c6bdc04c6334888cff00a6b3c3768bc9fbb6
Faizy51 <faizulla49@yahoo.co.uk>
2020-04-01 22:08:04 +0530

    Initial Commit

---
+++ VideoPlayerApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerApplication
+//
+//  Created by Faizyy on 01/04/20.
+//  Copyright © 2020 faiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
